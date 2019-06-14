b3d84c6e7149314fc5d0f1fa331078fef0f11f8c
teramotodaiki <i101323@gmail.com>
2019-06-15 02:48:09 +0900

    feat: stop audio and video

--- youtube.js
+++ youtube.js
@@ -1,1 +1,16 @@
-console.log("You are watching youtube");
+const notice = "【制限中】６時を過ぎたら、見ることが出来るようになります";
+
+function stopWatching() {
+  const hours = new Date().getHours();
+  if (18 <= hours && hours < 24) return; // ok
+
+  const video = document.querySelector("video");
+  video.style.display = "none";
+  video.muted = true;
+  const title = document.querySelector(".title");
+  title.textContent = notice;
+
+  requestIdleCallback(stopWatching);
+}
+
+requestIdleCallback(stopWatching);
